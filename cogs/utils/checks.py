e6f529d5770fbf0c7df360efc993b05e1ae7135c
Logan <75159044+lharlan@users.noreply.github.com>
2023-02-12 11:28:59 -0700

    Update to allow Sec owner access

--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -3,13 +3,14 @@ from discord.ext import commands
 
 logan_id = 462273017284919326
 madweeb_id = 734119924464746637
+sec_id = 571475470940504065
 
 real_admin = 711901311234474054
 real_mod = 711901391555264514
 
 def is_logan():
     async def predicate(ctx):
-        if ctx.author.id == logan_id or ctx.author.id == madweeb_id:
+        if ctx.author.id == logan_id or ctx.author.id == madweeb_id or ctx.author.id == sec_id:
             return True
     return commands.check(predicate)
 
